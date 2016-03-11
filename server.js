3bc0cc076bc5a30bf084c853561009425640dfbb
Mats Tornberg <mats@cavcms.se>
2016-03-11 10:38:52 +0100

    static server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// The number of milliseconds in one day
+var oneDay = 1;//86400000;
+//var oneDay = 86400000;
+
+// Use compress middleware to gzip content
+app.use(express.compress());
+
+// Serve up content from public directory
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.listen(80);
