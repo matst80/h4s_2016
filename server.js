dce82c41c82ffa4998ec7c5f1dab51d910e976c5
Mats Tornberg <mats@cavcms.se>
2016-03-11 10:47:29 +0100

    nytt i express

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-var express = require('express');
-var app = express();
+var connect = require('connect');
+var http = require('http');
 
-// The number of milliseconds in one day
-var oneDay = 1;//86400000;
-//var oneDay = 86400000;
+var app = connect();
 
-// Use compress middleware to gzip content
-app.use(express.compress());
+// gzip/deflate outgoing responses
+var compression = require('compression');
+app.use(compression());
 
-// Serve up content from public directory
+// store session state in browser cookie
+/*var cookieSession = require('cookie-session');
+app.use(cookieSession({
+    keys: ['secret1', 'secret2']
+}));
+*/
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+// respond to all requests
+app.use(function(req, res){
+  res.end('Hello from Connect!\n');
+});
+
+//create node.js http server and listen on port
+// Serve up content from public directory
+
 
 app.listen(80);
